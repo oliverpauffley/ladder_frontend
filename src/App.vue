4b014020f6c9ad9dec19e9b8f55174e79bedd007
oliverpauffley <mrpauffley@gmail.com>
2019-07-04 16:28:37 +0000

    First commit

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,19 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <TheHeader></TheHeader>
     <router-view />
   </div>
 </template>
+
+<script>
+import TheHeader from "./components/TheHeader";
+
+export default {
+  components: {
+    TheHeader
+  }
+};
+</script>
 
 <style>
 #app {
@@ -15,17 +22,5 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
 }
 </style>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <b-navbar toggleable="lg" variant="dark">
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav id="nav">
+          <b-nav-item><router-link to="/">Home</router-link></b-nav-item>
+          <b-nav-item><router-link to="/login">Login</router-link></b-nav-item>
+          <b-nav-item
+            ><router-link to="/register">Register</router-link></b-nav-item
+          >
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+  </div>
+</template>
+
+<style>
+#nav {
+  padding: 15px;
+  color: ghostwhite;
+}
+
+#nav a {
+  font-weight: bold;
+  color: aliceblue;
+}
+</style>
